954683bbced704b55e06577ca4391edeaf64d5c9
Macy Parker <>
2019-03-14 13:47:42 -0400

    Add stuff to loader and Instruct text file

--- Loader.java
+++ Loader.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 // Loader
 public class Loader {
+    public static void main(String args[])
+    {
+        readProgramFile();
+    }
+
+    public void readProgramFile()
+    {
+        BufferedReader reader;
+        try
+        {
+            reader = new BufferedReader(new FileReader(CS3502-OS-Project/ProgramFile.txt));
+
+        }
+    } // end of readProgramFile()
+
 }
