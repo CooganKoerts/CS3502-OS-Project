4ac13a92dabdfeb2324c67974b1c8c8a632cfdbc
Coogan M Koerts <35974529+CooganKoerts@users.noreply.github.com>
2019-03-14 14:21:00 -0400

    Add files via upload

--- Process.java
+++ Process.java
@@ -2,16 +2,18 @@ import java.util.Random;
 
 public class Process {
 
-    private int processId = 0;
-    private String processName = "";
-    private int processSize = 0;// generate random size in kb
-    private int processPriority = 0; // generate random priority (1-3, 1 being highest priority 3 being lowest)
+    private int processId;
+    private String processName;
+    private String processJob;
+    private int processSize;// generate random size in kb
+    private int processPriority; // generate random priority (1-3, 1 being highest priority 3 being lowest)
 
-    public Process(int processId, String processName) {
+    public Process(int processId, String processName, String processJob ,int processSize, int processPriority) {
         this.processId = processId;
         this.processName = processName;
-        this.processSize = (int)(Math.random() * 100 + 1);
-        this.processPriority = (int)(Math.random() * 3 + 1);
+        this.processJob = processJob;
+        this.processSize = processSize;
+        this.processPriority = processPriority;
     }
 
     public int getProcessId() {
