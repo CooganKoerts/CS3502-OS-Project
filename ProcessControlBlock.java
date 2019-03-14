c6974c706b87dc6f192405b55b524f3a388f9e8a
Macy Parker <>
2019-03-14 15:46:24 -0400

    Updates on loader and PCB

---
+++ ProcessControlBlock.java
@@ -0,0 +1,8 @@
+public class ProcessControlBlock {
+    String jobID; // hex number
+    String numOfWords; // hex number
+    String priority; // hex number
+    String inputBufferSize; // hex number
+    String outputBufferSize; // hex number
+    String tempBufferSize; // hex number
+}
