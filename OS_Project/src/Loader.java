de0d96bc354eda7403a0e22163fb3dd582dcc542
Macy Parker <>
2019-03-14 20:06:12 -0400

    Changes to PCB and Loader

--- OS_Project/src/Loader.java
+++ OS_Project/src/Loader.java
@@ -1,18 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class Loader {
+    LinkedList<ProcessControlBlock> queue;
+    Disk disk;
 
     // This is here for running and testing this class' methods
-    public static void main(String args[])
+    public void main(String args[])
     {
         readProgramFile();
     }
 
-    private static void readProgramFile()
+    private void readProgramFile()
     {
-
+        String attributes = "";
         BufferedReader reader;
         try
         {
@@ -30,14 +33,18 @@ public class Loader {
                 {
                     if (line.contains("JOB"))
                     {
-                        String jobID;
-                        String numOfWords;
-                        String priority_num;
-                        String[] splitLine = line.split("\\s+");
-                        jobID = splitLine[2];
-                        numOfWords = splitLine[3];
-                        priority_num = splitLine[4];
+                        attributes = line + " ";
                     }
+                    else if (line.contains("Data"))
+                    {
+                        attributes += line;
+                        addJobToPCB(attributes);
+                        attributes = ""; // reset attributes
+                    }
+                }
+                else if (!line.contains("//"))
+                {
+                    disk.addToDisk(line);
                 }
                 line = reader.readLine(); // reads next line
             }
@@ -49,5 +56,29 @@ public class Loader {
         }
 
     } // end of readProgramFile()
+
+    /*
+    Add Process Control block for new process to queue object
+    String format: // JOB a b c // Data d e f
+     */
+    private void addJobToPCB(String data)
+    {
+        String jobID;
+        String numOfWords;
+        String priority_num;
+        String inputBuff;
+        String outputBuff;
+        String tempBuff;
+
+        String[] splitLine = data.split("\\s+");
+        jobID = splitLine[2];
+        numOfWords = splitLine[3];
+        priority_num = splitLine[4];
+        inputBuff = splitLine[7];
+        outputBuff = splitLine[8];
+        tempBuff = splitLine[9];
+
+        queue.add(new ProcessControlBlock(jobID, numOfWords, priority_num, inputBuff, outputBuff, tempBuff));
+    }
 
 }
--- OS_Project/src/ProcessControlBlock.java
+++ OS_Project/src/ProcessControlBlock.java
@@ -5,4 +5,15 @@ public class ProcessControlBlock {
     String inputBufferSize; // hex number
     String outputBufferSize; // hex number
     String tempBufferSize; // hex number
+
+    public ProcessControlBlock(String job_id, String num_of_words, String _priority, String input_buffer_size,
+                               String output_buffer_size, String temp_buffer_size)
+    {
+        this.jobID = job_id;
+        this.numOfWords = num_of_words;
+        this.priority = _priority;
+        this.inputBufferSize = input_buffer_size;
+        this.outputBufferSize = output_buffer_size;
+        this.tempBufferSize = temp_buffer_size;
+    }
 }
