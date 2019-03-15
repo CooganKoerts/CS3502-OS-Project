2243489126db69daee616cec4eac493cfa533402
Coogan M Koerts <35974529+CooganKoerts@users.noreply.github.com>
2019-03-14 20:22:33 -0400

    Add files via upload

--- MemoryManager.java
+++ MemoryManager.java
@@ -2,11 +2,15 @@
 
     Memory manager class will be in-charge of manipulating memory addresses and registers
 
+
  */
 
 public class MemoryManager {
-    private Memory memory = new Memory();
+    private Memory memory = new Memory(10, 10);
     private int[] registers = memory.getRegisters();
+
+    public MemoryManager() {
+    }
 
     public int getRegisterSize() {
         return memory.sizeOfMemory();
@@ -24,7 +28,18 @@ public class MemoryManager {
         registers[n] = value;
     }
 
-    public void writeToRAM() { }
-    public void writeToDisk() { }
+    //public void writeToRAM() { }
+
+    public void writeToDisk(String job) {
+        memory.getDisk().addToDisk(job);
+    }
+
+    public void removeFromDisk(String job) {
+        memory.getDisk().removeFromDisk(job);
+    }
+
+    public String getProcessFromDisk(String job) {
+        return memory.getDisk().accessDisk(job);
+    }
 
 }
