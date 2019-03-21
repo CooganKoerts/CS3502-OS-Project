3bbfd4568a29857e66a8b665017bca0625537ab8
Coogan M Koerts <35974529+CooganKoerts@users.noreply.github.com>
2019-03-21 13:54:01 -0400

    Update Dispatcher.java

--- OS_Project/src/Dispatcher.java
+++ OS_Project/src/Dispatcher.java
@@ -1,2 +1,9 @@
+/*
+    The Dispatcher method assigns a process to the CPU. It is also responsible for context switching of
+jobs when necessary (more on this later!). For now, the dispatcher will extract parameter data from the
+PCB and accordingly set the CPU’s PC, and other registers, before the OS calls the CPU to execute
+the job.
+*/
+
 public class Dispatcher {
 }
