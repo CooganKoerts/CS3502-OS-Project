4e53478bf6ae8065bfc7cb7fe76fb6b88e7f40b6
Macy Parker <>
2019-04-09 13:56:40 -0400

    Decode method added - almost finished

--- OS_Project/src/CPU.java
+++ OS_Project/src/CPU.java
@@ -2,6 +2,8 @@
     This is our CPU file
  */
 
+import java.util.ArrayList;
+
 public class CPU
 {
     public int[] registers = new int[16];
@@ -78,7 +80,7 @@ public class CPU
         return result;
     }
 
-    public void decode(String instruction) {
+    public void ConvertInstruct(String instruction) {
         String binary_instructions = hex_to_binary(instruction.substring(2));
         String temporary_instructions = binary_instructions;
         instant = Integer.parseInt(temporary_instructions.substring(0, 2));
@@ -117,20 +119,94 @@ public class CPU
             }
         }
     }
-    
-    public void execute() {}
 
     /*
-    Notes for effective address method
-    // disk is 2048 words long
-    // ram is 1024 words long
-    // each word is 4 bytes & 8 hex chars
-    // The scheduler must note in the PCB, which physical addresses
-    // in RAM each program/job begins, and ends.
-    // This ‘start’ address must be stored in the base-register (or program-counter) of the job)
-    // store the start addresses of the input-buffer and output-buffer spaces allocated in RAM as well
-    // When a process is run, the base register is loaded with the physical location where the process begins in memory
+    Returns array of strings InstructionSet
+    Each string is a binary number except for any OPCODE values,
+    which should be converted to hex already in this method
+
+    When parsing through this, start by checking InstructionSet[0]
+    either equals 00, 01, 10, or 11. take indexes based on how many values
+    come with that instruction (opcode, reg, dreg, address, etc) and when
+    those are taken, go to the next index and check if 00, 01, 10, 11,
+    and repeat this.
      */
+    public ArrayList<String> decode(int binaryInstruct)
+    {
+        ArrayList<String> InstructionSet = new ArrayList<String>();
+
+        // Format as string so it can be split
+        String binaryInstructStr = String.valueOf(binaryInstruct);
+
+        String firstTwoBits = binaryInstructStr.substring(0, 2); // first 2 bits
+        String lastThirty = binaryInstructStr.substring(2); // remaining 30 bits
+
+        // Variables for components of instruction
+        String OPcode = "";
+        String sReg1 = ""; // Source register - 4 bits
+        String sReg2 = ""; // Source Register - (another) 4 bits
+        String dReg = ""; // Destination Register - 4 bits
+        String bReg = ""; // Base Register - 4 bits
+        String address = ""; // Address may at times be treated as data, which is
+                            // direct addressing, if not calculate indirect ?
+        String reg1 = "";
+        String reg2 = "";
+
+        // Instruction Type
+        if (firstTwoBits.equals("00")) // Arithmetic
+        {
+            OPcode = lastThirty.substring(0, 6);
+            // CONVERT OPCODE TO HEX HERE
+            sReg1 = lastThirty.substring(6, 10);
+            sReg2 = lastThirty.substring(10, 14);
+            dReg = lastThirty.substring(14, 18);
+            // 000000000000 not used
+            InstructionSet.add("00");
+            InstructionSet.add(OPcode);
+            InstructionSet.add(sReg1);
+            InstructionSet.add(sReg2);
+            InstructionSet.add(dReg);
+        }
+        else if (firstTwoBits.equals("01")) // Conditional branch and Immediate format
+        {
+            OPcode = lastThirty.substring(0, 6);
+            // CONVERT OPCODE TO HEX HERE
+            bReg = lastThirty.substring(6, 10);
+            dReg = lastThirty.substring(10, 14);
+            address = lastThirty.substring(14);
+            InstructionSet.add("01");
+            InstructionSet.add(OPcode);
+            InstructionSet.add(bReg);
+            InstructionSet.add(dReg);
+            InstructionSet.add(address);
+        }
+        else if (firstTwoBits.equals("10")) // Unconditional Jump
+        {
+            OPcode = lastThirty.substring(0, 6);
+            // CONVERT OPCODE TO HEX HERE
+            address = lastThirty.substring(6);
+            InstructionSet.add("10");
+            InstructionSet.add(OPcode);
+            InstructionSet.add(address);
+
+        }
+        else if (firstTwoBits.equals("11"))
+        {
+            OPcode = lastThirty.substring(0, 6);
+            // CONVERT OPCODE TO HEX HERE
+            reg1 = lastThirty.substring(6, 10);
+            reg2 = lastThirty.substring(10, 14);
+            address = lastThirty.substring(14);
+            InstructionSet.add("11");
+            InstructionSet.add(OPcode);
+            InstructionSet.add(reg1);
+            InstructionSet.add(reg2);
+            InstructionSet.add(address);
+        }
+        return InstructionSet;
+    }
+    
+    public void execute() {}
     
     /*
     Param AddressingMode a: Direct or Indirect
